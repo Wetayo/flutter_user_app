d71bbd96bf0f69b60a65c35b7615fabcb9392f64
ksssssw <rtd0731@naver.com>
2021-03-23 17:45:40 +0900

    backup

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:wetayo_app/api/config.dart';
+import 'package:wetayo_app/screen/ServerTest.dart';
+import 'package:wetayo_app/screen/detail_page.dart';
+import 'package:wetayo_app/screen/home_screen.dart';
+import 'package:wetayo_app/screen/lbs_screen.dart';
+import 'package:wetayo_app/screen/mutationTest.dart';
+import 'package:wetayo_app/screen/station_screen.dart';
+import 'package:wetayo_app/screen/test.dart';
+import 'package:wetayo_app/widget/bottom_bar.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  TabController controller;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'WeTayo',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.black,
+        accentColor: Colors.white,
+      ),
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          body: TabBarView(
+            physics: NeverScrollableScrollPhysics(),
+            children: <Widget>[
+              HomeScreen(),
+              //
+              GraphQLProvider(
+                client: graphqlService.client,
+                child: MutationTest(),
+              ),
+              GraphQLProvider(
+                client: graphqlService.client,
+                child: StationScreen(),
+              )
+            ],
+          ),
+          bottomNavigationBar: Bottom(),
+        ),
+      ),
+    );
+  }
+}
